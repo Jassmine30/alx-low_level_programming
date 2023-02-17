3cbddb97278f11363c2451234c83af88f243d11c
Jassmine30 <jassminekeamogetswe.matlala@gmail.com>
2023-02-17 03:27:07 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "with proper grammer, but the outcome is a piece of art,"
+ * followed by a new line.
+ * Return: 0.
+ */
+
+int main(void)
+
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+
+}
