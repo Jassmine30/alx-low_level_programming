f67ca4c10fdd27cfc62caa032b2157930ef987b6
Jassmine30 <jassminekeamogetswe.matlala@gmail.com>
2023-02-17 03:34:45 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 
 
 /**
- * main - Prints "with proper grammer, but the outcome is a piece of art,"
- * followed by a new line.
- * Return: 0.
+ * main - Prints "with proper grammer, but the outcome is a piece of art," 
+ * follwed by a new line.
+ * Return: Always 0.
  */
 
 int main(void)
